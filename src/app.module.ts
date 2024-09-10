57c912109d36c24bd4e5f6455d44fed35c7530a4
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-10 12:32:23 +0200

    Remove boilerplate code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -9,7 +7,5 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
