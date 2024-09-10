667c16b1f43959b1b0875435cdff30984be0fa0e
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-10 18:51:55 +0200

    Add http module

--- src/modules/trips/trips.module.ts
+++ src/modules/trips/trips.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TripsController } from './trips.controller';
 import { TripsService } from './service/trips.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [TripsController],
   providers: [TripsService],
 })
