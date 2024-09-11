5e8a6b73acdd215e78062a7bc7a240ce6be1c13a
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-11 18:32:33 +0200

    Configure validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const listenPort = configService.get('serverPort');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   app.setGlobalPrefix('api');
 
   await app.listen(listenPort);
