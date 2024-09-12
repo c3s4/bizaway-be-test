7420df8c208d2bb569a1f2d3dce87955e0e47119
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-12 11:49:20 +0200

    Add e2e test for trip search

--- test/modules/trips/trips.e2e-spec.ts
+++ test/modules/trips/trips.e2e-spec.ts
@@ -28,8 +28,59 @@ describe('[Feature] Trips - /trips', () => {
   });
 
   describe('GET /search', () => {
-    it('should return an empty array response', async () => {
+    it('should fail because of not valid data', async () => {
       await request(app.getHttpServer()).get('/api/trips/search').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=wrong').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=&destination=').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=wrong&destination=wrong').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=&destination=wrong').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=BCN&destination=wrong').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=BCN&destination=').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=wrong&destination=BCN').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=&destination=BCN').expect(400);
+      await request(app.getHttpServer()).get('/api/trips/search?origin=BCN&destination=IST&sort_by=wrong').expect(400);
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=cheapest&page=0')
+        .expect(400);
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=cheapest&page=-1')
+        .expect(400);
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=cheapest&items_per_page=0')
+        .expect(400);
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=cheapest&items_per_page=-1')
+        .expect(400);
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=cheapest&page=')
+        .expect(400);
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=cheapest&items_per_page=')
+        .expect(400);
+    });
+
+    it('should complete successfully', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST')
+        .expect(200);
+      expect(response.body).toHaveProperty('currentPage');
+      expect(response.body).toHaveProperty('totalPages');
+      expect(response.body).toHaveProperty('totalItems');
+      expect(response.body).toHaveProperty('itemsPerPage');
+      expect(response.body).toHaveProperty('items');
+      expect(Array.isArray(response.body.items)).toBeTruthy();
+
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=fastest')
+        .expect(200);
+
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=BCN&destination=IST&sort_by=fastest&page=1&items_per_page=10')
+        .expect(200);
+
+      await request(app.getHttpServer())
+        .get('/api/trips/search?origin=IST&destination=IST&sort_by=fastest&page=1&items_per_page=10')
+        .expect(200);
     });
   });
 
