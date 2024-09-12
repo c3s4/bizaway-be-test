8c5c1ec9542005f8d0892157d29b2acab1332283
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-12 10:17:03 +0200

    Add controller search method to create the real endpoint

--- src/modules/trips/trips.controller.spec.ts
+++ src/modules/trips/trips.controller.spec.ts
@@ -1,18 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TripsController } from './trips.controller';
+import { TripsService } from './service/trips.service';
+import { PlaceCode, SearchTripsRequestDto, SortBy } from './dtos/search_trips.dto';
+import { BadRequestException } from '@nestjs/common';
 
+const trips = [
+  {
+    origin: 'BCN',
+    destination: 'LAX',
+    cost: 159,
+    duration: 39,
+    type: 'train',
+    remoteId: 'a5b0e087-ad0a-41e7-94c4-00634a222823',
+    display_name: 'from BCN to LAX by train',
+  },
+  {
+    origin: 'BCN',
+    destination: 'LAX',
+    cost: 282,
+    duration: 38,
+    type: 'car',
+    remoteId: 'ca89c6d5-445c-4f7f-aa4b-d4860bcf1d31',
+    display_name: 'from BCN to LAX by car',
+  },
+];
+
+const mockTripsService = () => ({
+  searchTripsFromIntegration: jest.fn().mockResolvedValue({
+    items: trips,
+    totalItems: 3,
+    currentPage: 1,
+    totalPages: 2,
+    itemsPerPage: 2,
+  }),
+});
 describe('TripsController', () => {
   let controller: TripsController;
+  let tripService: TripsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TripsController],
+      providers: [
+        {
+          provide: TripsService,
+          useFactory: mockTripsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TripsController>(TripsController);
+    tripService = module.get<TripsService>(TripsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a list of trips', async () => {
+    let searchParams: SearchTripsRequestDto = {
+      origin: PlaceCode.BCN,
+      destination: PlaceCode.LAX,
+    };
+    const foundTrips = await controller.searchTrips(searchParams);
+    expect(foundTrips).toEqual({
+      items: trips,
+      totalItems: 3,
+      currentPage: 1,
+      totalPages: 2,
+      itemsPerPage: 2,
+    });
+
+    expect(tripService.searchTripsFromIntegration).toHaveBeenCalledWith(searchParams);
+
+    searchParams = {
+      origin: PlaceCode.BCN,
+      destination: PlaceCode.LAX,
+      sortBy: SortBy.CHEAPEST,
+    };
+
+    await controller.searchTrips(searchParams);
+    expect(tripService.searchTripsFromIntegration).toHaveBeenCalledWith(searchParams);
+
+    searchParams = {
+      origin: PlaceCode.BCN,
+      destination: PlaceCode.LAX,
+      page: 2,
+    };
+    await controller.searchTrips(searchParams);
+    expect(tripService.searchTripsFromIntegration).toHaveBeenCalledWith(searchParams);
+
+    searchParams = {
+      origin: PlaceCode.BCN,
+      destination: PlaceCode.LAX,
+      page: 2,
+      itemsPerPage: 1,
+    };
+    await controller.searchTrips(searchParams);
+    expect(tripService.searchTripsFromIntegration).toHaveBeenCalledWith(searchParams);
+
+    searchParams = {
+      origin: PlaceCode.BCN,
+      destination: PlaceCode.LAX,
+      page: 2,
+      itemsPerPage: 1,
+      sortBy: SortBy.FASTEST,
+    };
+    await controller.searchTrips(searchParams);
+    expect(tripService.searchTripsFromIntegration).toHaveBeenCalledWith(searchParams);
+  });
+
+  it('should throw an error if the page number is too high', async () => {
+    const searchParams: SearchTripsRequestDto = {
+      origin: PlaceCode.BCN,
+      destination: PlaceCode.LAX,
+      page: 3,
+    };
+    const foundTrips = controller.searchTrips(searchParams);
+    expect(tripService.searchTripsFromIntegration).toHaveBeenCalledWith(searchParams);
+    await expect(foundTrips).rejects.toThrow(BadRequestException);
   });
 });
--- src/modules/trips/trips.controller.ts
+++ src/modules/trips/trips.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Logger, Query } from '@nestjs/common';
+import { SearchTripsListResponseDto, SearchTripsRequestDto } from './dtos/search_trips.dto';
+import { TripsService } from './service/trips.service';
 
 @Controller('trips')
 export class TripsController {
-  constructor() {}
+  private logger = new Logger(TripsController.name);
+  constructor(private tripService: TripsService) {}
 
-  @Get()
-  getAllTrips() {
-    return 'All trips';
+  @Get('/search')
+  async searchTrips(@Query() searchParams: SearchTripsRequestDto): Promise<SearchTripsListResponseDto> {
+    const foundTrips = await this.tripService.searchTripsFromIntegration(searchParams);
+    if (foundTrips && searchParams.page && foundTrips.totalPages < searchParams.page) {
+      throw new BadRequestException('Page number is too high');
+    }
+    return foundTrips;
   }
 }
