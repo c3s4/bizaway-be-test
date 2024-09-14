7f851e61d240cb05cee9b4aebb1e12313cd0abaa
Davide Cesaroni <davide.cesaroni@gmail.com>
2024-09-14 12:06:31 +0200

    Add endpoint for get trip by id

--- src/modules/trips/trips.controller.ts
+++ src/modules/trips/trips.controller.ts
@@ -20,6 +20,15 @@ export class TripsController {
   private logger = new Logger(TripsController.name);
   constructor(private tripService: TripsService) {}
 
+  @Get('/search')
+  async searchTrips(@Query() searchParams: SearchTripsRequestDto): Promise<SearchTripsListResponseDto> {
+    const foundTrips = await this.tripService.searchTripsFromIntegration(searchParams);
+    if (foundTrips && searchParams.page && searchParams.page > 1 && foundTrips.totalPages < searchParams.page) {
+      throw new BadRequestException('Page number is too high');
+    }
+    return foundTrips;
+  }
+
   @Post('/')
   async saveTrip(@Body() saveTripRequest: SaveTripRequestDto): Promise<SaveTripResponseDto> {
     return await this.tripService.saveTrip(saveTripRequest);
@@ -30,18 +39,14 @@ export class TripsController {
     return await this.tripService.getTrips(getTripsParams);
   }
 
+  @Get('/:id')
+  async getTripById(@Param('id') id: string): Promise<SaveTripResponseDto> {
+    return await this.tripService.getTripById(id);
+  }
+
   @Delete('/:id')
   @HttpCode(204)
   async deleteTripById(@Param('id') id: string) {
     await this.tripService.deleteTripById(id);
-  }
-
-  @Get('/search')
-  async searchTrips(@Query() searchParams: SearchTripsRequestDto): Promise<SearchTripsListResponseDto> {
-    const foundTrips = await this.tripService.searchTripsFromIntegration(searchParams);
-    if (foundTrips && searchParams.page && searchParams.page > 1 && foundTrips.totalPages < searchParams.page) {
-      throw new BadRequestException('Page number is too high');
-    }
-    return foundTrips;
   }
 }
